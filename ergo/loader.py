f989b2d03d6cb94cd5b959955d1c1c39216fe1bb
evilsocket <evilsocket@gmail.com>
2018-11-24 12:05:08 +0100

    misc: small fix or general refactoring i did not bother commenting

--- ergo/loader.py
+++ ergo/loader.py
@@ -1,18 +1,33 @@
 import logging as log
 import pandas as pd
 import numpy as np
+import threading
 
 class Loader(object):
     def __init__(self, dataset):
         self.dataset = dataset
 
+    def _worker(self, idx):
+        if idx == 0:
+            log.info("loading %s ..." % self.dataset.train_path)
+            self.dataset.train = pd.read_csv(self.dataset.train_path, sep = ',', header = None)
+        elif idx == 1:
+            log.info("loading %s ..." % self.dataset.test_path)
+            self.dataset.test = pd.read_csv(self.dataset.test_path, sep = ',', header = None)
+        elif idx == 2:
+            log.info("loading %s ..." % self.dataset.valid_path)
+            self.dataset.validation = pd.read_csv(self.dataset.valid_path, sep = ',', header = None)
+
     def load(self):
-        log.info("loading %s ..." % self.dataset.train_path)
-        self.dataset.train = pd.read_csv(self.dataset.train_path, sep = ',', header = None)
-        log.info("loading %s ..." % self.dataset.test_path)
-        self.dataset.test = pd.read_csv(self.dataset.test_path, sep = ',', header = None)
-        log.info("loading %s ..." % self.dataset.valid_path)
-        self.dataset.validation = pd.read_csv(self.dataset.valid_path, sep = ',', header = None)
+        threads = ( \
+          threading.Thread(target=self._worker, args=(0,)),
+          threading.Thread(target=self._worker, args=(1,)),
+          threading.Thread(target=self._worker, args=(2,)) 
+        )
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join() 
 
         u = np.concatenate( \
                 (self.dataset.validation.iloc[:,0].unique(), 
