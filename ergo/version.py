516acf03909dedda2b41e6daaf6f54f8c560d9ec
evilsocket <evilsocket@gmail.com>
2018-11-24 13:45:27 +0100

    Releasing v1.1.0

--- ergo/version.py
+++ ergo/version.py
@@ -3,6 +3,6 @@ __email__     = 'evilsocket@gmail.com'
 __copyright__ = 'Copyright 2018, Simone Margaritelli'
 __credits__   = ['Nico Chiaraviglio']
 __license__   = 'GPL'
-__version__   = '1.0.0'
+__version__   = '1.1.0'
 __status__    = 'Production'
 
