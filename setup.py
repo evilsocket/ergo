fa90a8c059fdda28bae014c44be51d77dc0fdbd9
evilsocket <evilsocket@gmail.com>
2019-04-23 17:59:18 +0200

    misc: small fix or general refactoring i did not bother commenting

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ import os
 
 try:
     long_description = open( 'README.md', 'rt' ).read()
+    ctype = 'text/markdown'
 except:
     long_description = 'ergo is a tool that makes deep learning with Keras easier.'
+    ctype = 'text/plain'
 
 setup( name                 = 'ergo',
        version              = __version__,
        description          = long_description,
        long_description     = long_description,
+       long_description_content_type = ctype,
        author               = __author__,
        author_email         = __email__,
        url                  = 'http://www.github.com/evilsocket/ergo',
