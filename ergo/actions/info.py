f11ac588ce167992a8b6bb395111505bc592d28e
evilsocket <evilsocket@gmail.com>
2019-05-25 15:12:35 +0200

    new: ergo info has now an optional -j/--to-json argument to print info in json format

--- ergo/actions/info.py
+++ ergo/actions/info.py
@@ -1,3 +1,5 @@
+import argparse
+import json
 import sys
 stderr = sys.stderr
 sys.stderr = open('/dev/null', 'w')
@@ -33,12 +35,43 @@ def mem_fmt(num, suffix='B'):
         num /= 1024.0
     return "%.1f %s%s" % (num, 'Yi', suffix)
 
-def action_info(argc, argv):
-    print(banner.strip("\n") % (__version__, keras.__version__, tf.__version__))
-    print("")
-    print("Hardware:\n")
+def parse_args(argv):
+    parser = argparse.ArgumentParser(prog="ergo info", description="Print library versions and hardware info.",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    devs = device_lib.list_local_devices()
-    npad, tpad  = get_pads(devs)
-    for dev in devs:
-        print( "%s (%s) - %s" % (dev.name.ljust(npad,' '), dev.device_type.ljust(tpad,' '), mem_fmt(dev.memory_limit)))
+    parser.add_argument("-j", "--to-json", dest="to_json", action="store_true", default=False,
+        help="Output the information to json instead of text.")
+
+    args = parser.parse_args(argv)
+    return args
+
+def action_info(argc, argv):
+    args = parse_args(argv)
+
+    if args.to_json is True:
+        devices = []
+        info = {
+            "version": __version__,
+            "keras_version": keras.__version__,
+            "tf_version": tf.__version__,
+            "devices":[]
+        }
+        devs = device_lib.list_local_devices()
+        for dev in devs:
+            info['devices'].append({
+                'name': dev.name,
+                'type': dev.device_type,
+                'memory': dev.memory_limit,
+                'description': dev.physical_device_desc
+            })
+        
+        print(json.dumps(info)) 
+    else:
+        print(banner.strip("\n") % (__version__, keras.__version__, tf.__version__))
+        print("")
+        print("Hardware:\n")
+
+        devs = device_lib.list_local_devices()
+        npad, tpad  = get_pads(devs)
+        for dev in devs:
+            print( "%s (%s) - %s" % (dev.name.ljust(npad,' '), dev.device_type.ljust(tpad,' '), mem_fmt(dev.memory_limit)))
