cd56f4f99ad9323757945971322dda7b7a9531fa
nicochidt <nico.chiaraviglio@gmail.com>
2020-04-03 12:49:23 +0200

    Add multi model tracking

---
+++ ergo/core/multi_model.py
@@ -0,0 +1,18 @@
+
+class multi_model():
+    def __init__ (self, cpu_model, gpu_model = None):
+        self.cpu_model = cpu_model
+        self.gpu_model = gpu_model
+
+    def fit (self, x, y, **kwargs):
+        return self.gpu_model.fit(x, y, **kwargs)
+
+    def compile(self, **kwargs):
+        return self.gpu_model.compile(**kwargs)
+
+    def __getattr__(self, name, *args, **kwargs):
+        if hasattr(self.cpu_model, name):
+            def wrapper(*args, **kwargs):
+                return getattr(self.cpu_model, name)(*args)
+            return wrapper
+        raise AttributeError("Method/Attribute %s not found for keras model" % name)
